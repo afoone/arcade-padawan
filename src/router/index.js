ee010c29d4c917676ec38bd612351a0956b75887
arabbuc <abarmando@gmail.com>
2020-07-16 11:59:29 +0200

    errores varios

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+import { userSetter } from 'core-js/fn/symbol'
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Usuario from '../views/Usuario.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,17 @@ Vue.use(VueRouter)
     component: Home
   },
   {
-    path: '/about',
+    path: '/acerca-de',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/user',
+    name: 'Usuario',
+    component: Usuario
   }
 ]
 
---
+++ src/views/Usuario.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+      <User msg="Hola, soy un SUPER USUARIO  Me llamo Groot"/>
+        <h2> Vista Usuarios</h2>
+    </div>
+</template>
+<script>
+// @ is an alias to /src
+import User from '@/components/Usuario.vue'
+
+export default {
+  name: 'User',
+  components: {
+    User
+  }
+}
+</script>
